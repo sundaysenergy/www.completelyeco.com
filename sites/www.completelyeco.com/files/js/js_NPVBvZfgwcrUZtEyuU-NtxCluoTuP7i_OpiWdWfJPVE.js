98b5fd0880b2959dba49c0fda3e6cbbea4318c74
Kai Curry <kai@sundaysenergy.com>
2014-12-05 10:02:24 -0600

    init commit

---
+++ sites/www.completelyeco.com/files/js/js_NPVBvZfgwcrUZtEyuU-NtxCluoTuP7i_OpiWdWfJPVE.js
@@ -0,0 +1,102 @@
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Open links to Mollom.com in a new window.
+ *
+ * Required for valid XHTML Strict markup.
+ */
+Drupal.behaviors.mollomTarget = {
+  attach: function (context) {
+    $(context).find('.mollom-target').click(function () {
+      this.target = '_blank';
+    });
+  }
+};
+
+/**
+ * Attach click event handlers for CAPTCHA links.
+ */
+Drupal.behaviors.mollomCaptcha = {
+  attach: function (context, settings) {
+    // @todo Pass the local settings we get from Drupal.attachBehaviors(), or
+    //   inline the click event handlers, or turn them into methods of this
+    //   behavior object.
+    $('a.mollom-switch-captcha', context).click(getMollomCaptcha);
+  }
+};
+
+/**
+ * Fetch a Mollom CAPTCHA and output the image or audio into the form.
+ */
+function getMollomCaptcha() {
+  // Get the current requested CAPTCHA type from the clicked link.
+  var newCaptchaType = $(this).hasClass('mollom-audio-captcha') ? 'audio' : 'image';
+
+  var context = $(this).parents('form');
+
+  // Extract the form build ID and Mollom content ID from the form.
+  var formBuildId = $('input[name="form_build_id"]', context).val();
+  var mollomContentId = $('input.mollom-content-id', context).val();
+
+  var path = 'mollom/captcha/' + newCaptchaType + '/' + formBuildId;
+  if (mollomContentId) {
+    path += '/' + mollomContentId;
+  }
+
+  // Retrieve a new CAPTCHA.
+  $.ajax({
+    url: Drupal.settings.basePath + path,
+    type: 'POST',
+    dataType: 'json',
+    success: function (data) {
+      if (!(data && data.content)) {
+        return;
+      }
+      // Inject new CAPTCHA.
+      $('.mollom-captcha-content', context).parent().html(data.content);
+      // Update CAPTCHA ID.
+      $('input.mollom-captcha-id', context).val(data.captchaId);
+      // Add an onclick-event handler for the new link.
+      Drupal.attachBehaviors(context);
+      // Focus on the CAPTCHA input.
+      $('input[name="mollom[captcha]"]', context).focus();
+    }
+  });
+  return false;
+}
+
+})(jQuery);
+;
